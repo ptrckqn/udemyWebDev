598bb45a815791099a7b63e50fe24b34333429b8
Patrick Quan <patrickquan@Patricks-MBP.hitronhub.home>
2018-01-16 10:58:09 -0700

    Added Javascript Stalker Exercise

---
+++ Javascript Exercises/stalker.js
@@ -0,0 +1,7 @@
+var fName = prompt("Hello, what is your first name?");
+var lName = prompt("And your last name?");
+var age = prompt("And lastly, your age?");
+alert("Hello, " + fName + " " + lName + ".");
+console.log("I like the name " + fName + " " + lName);
+alert("Your age is: " + age);
+console.log("My age is also: " + age);
