cabe00faf13790b76f2f9c7de76f57140d7f270d
Patrick Quan <patrickquan@Patricks-MBP.hitronhub.home>
2018-01-18 10:57:23 -0700

    Added delete function

--- Javascript Exercises/list.js
+++ Javascript Exercises/list.js
@@ -6,12 +6,20 @@ var quit = false;
 while(!quit){
   var input = prompt("What would you like to do?");
   if (input === "list"){
-    console.log(todos);
+  todos.forEach(function(todo, i){
+    console.log("*************")
+    console.log(i + ": " + todo);
+  });
   }
 
   else if(input === "new"){
     var newTodo = prompt("Enter new Todo");
     todos.push(newTodo);
+  }
+
+  else if(input === "delete"){
+    var index = prompt("Enter index of todo to delete");
+    todos.splice(index, 1);
   }
 
   else if (input === "quit"){
